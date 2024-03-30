6d85e87a07169bf9dc0119c97f18cedf88ae478a
alfianvx <devsroot8@gmail.com>
2024-03-30 12:38:15 +0700

    feat: add static data for display dummy content

---
+++ src/data/place.ts
@@ -0,0 +1,22 @@
+export const places = [
+  {
+    name: "Tempat 1",
+    location: "alamat",
+  },
+  {
+    name: "Tempat 2",
+    location: "alamat",
+  },
+  {
+    name: "Tempat 3",
+    location: "alamat",
+  },
+  {
+    name: "Tempat 4",
+    location: "alamat",
+  },
+  {
+    name: "Tempat 5",
+    location: "alamat",
+  },
+];
