b4d1bdb1f55ece8d2b2273c7e276abd2a4c6b89b
alfianvx <devsroot8@gmail.com>
2024-03-30 12:33:44 +0700

    feat: add categories page

---
+++ src/app/(category)/populer/page.tsx
@@ -0,0 +1,9 @@
+export default function Populer() {
+  return (
+    <div className="w-full h-screen px-4 py-6">
+      <h1 className="text-balance font-semibold mb-2 text-lg">
+        Wisata popluer buat kamu ❤
+      </h1>
+    </div>
+  );
+}
---
+++ src/app/(category)/rekomendasi/page.tsx
@@ -0,0 +1,9 @@
+export default function Recomended() {
+  return (
+    <div className="w-full h-screen px-4 py-6">
+      <h1 className="text-balance font-semibold mb-2 text-lg">
+        Wisata rekomendasi buat kamu 🤞
+      </h1>
+    </div>
+  );
+}
