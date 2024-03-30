5e1b9859075c7ec705e307d00cfe7d4e3c314358
alfianvx <devsroot8@gmail.com>
2024-03-30 12:40:03 +0700

    fix: use components to display content insted native jsx code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,10 @@
-"use client";
-import {
-  IconBarsThree3,
-  IconBell,
-  IconLocation,
-  IconMailNotify,
-} from "@irsyadadl/paranoid";
-import Slider from "react-slick";
+import { IconBarsThree3, IconBell, IconLocation } from "@irsyadadl/paranoid";
 import Link from "next/link";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-const places = [
-  {
-    name: "Tempat 1",
-    location: "alamat",
-  },
-  {
-    name: "Tempat 2",
-    location: "alamat",
-  },
-  {
-    name: "Tempat 3",
-    location: "alamat",
-  },
-  {
-    name: "Tempat 4",
-    location: "alamat",
-  },
-  {
-    name: "Tempat 5",
-    location: "alamat",
-  },
-  {
-    name: "Tempat 6",
-    location: "alamat",
-  },
-];
-
-const settings = {
-  className: "center",
-  centerMode: true,
-  infinite: true,
-  speed: 1000,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  centerPadding: "40px",
-  arrows: false,
-  rows: 1,
-  slidesPerRow: 1,
-  slidesToShow: 1,
-};
+import CardSlider from "@/components/card-slider";
+import CardList from "@/components/card-list";
 
 export default function Home() {
   return (
@@ -58,17 +12,14 @@ export default function Home() {
       <section className="h-screen px-4 py-6">
         {/* HEADER */}
         <div className="flex items-center justify-between mb-5">
-          <button className="p-1 border rounded-full">
-            <IconBarsThree3 />
-          </button>
-          <div className="text-center">
-            <span className="text-xs text-slate-400">Lokasi saat ini</span>
+          <div>
+            <span className="text-xs">Lokasi saat ini</span>
             <div className="flex items-center space-x-1">
               <IconLocation className="w-4" />{" "}
               <p className="text-sm">Jakarta, Indonesia</p>
             </div>
           </div>
-          <button className="p-1 border rounded-full">
+          <button className="p-1">
             <IconBell />
           </button>
         </div>
@@ -80,54 +31,31 @@ export default function Home() {
             placeholder="cari ..."
           />
         </div>
-        {/* PILIHAN USER */}
+        {/* REKOMENDASI USER */}
         <div className="mb-5">
           <div className="flex items-center justify-between mb-4">
-            <h1>Pilihan untuk kamu</h1>
-            <Link href={""}>lihat semua</Link>
+            <h1 className="text-lg font-bold">Rekomendasi</h1>
+            <Link
+              className="text-sm font-medium text-slate-500"
+              href="/rekomendasi"
+            >
+              lihat semua
+            </Link>
           </div>
-          <section className="">
-            <Slider {...settings}>
-              {places.map((place, index) => (
-                <div
-                  className="my-6 bg-white border rounded-lg max-w-max"
-                  key={index}
-                >
-                  <div className="w-56 h-60 bg-slate-200"></div>
-                  <div className="flex flex-col items-center py-3">
-                    <h1 className="mb-1">{place.name}</h1>
-                    <div className="flex items-center space-x-2">
-                      <span className="text-xs">{place.location}</span>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </Slider>
-          </section>
+          <CardSlider />
         </div>
         {/* PILIHAN POPULER */}
         <div>
           <div className="flex items-center justify-between">
-            <h1>Pilihan Populer</h1>
-            <Link href={""}>lihat semua</Link>
+            <h1 className="text-lg font-bold">Pilihan Populer</h1>
+            <Link
+              className="text-sm font-medium text-slate-500"
+              href="/populer"
+            >
+              lihat semua
+            </Link>
           </div>
-          <section className="flex flex-col py-4 space-y-3">
-            {places.map((place, index) => (
-              <div
-                key={index}
-                className="flex w-full p-3 space-x-4 border rounded-lg"
-              >
-                <div className="w-20 h-20 rounded-lg bg-slate-200"></div>
-                <div>
-                  <Link href={`/wisata/${place.name}`}>{place.name}</Link>
-                  <div className="flex items-center space-x-2">
-                    <IconLocation className="w-3 h-3" />
-                    <span className="text-xs">{place.location}</span>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </section>
+          <CardList />
         </div>
       </section>
     </main>
