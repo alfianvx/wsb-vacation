ea20985fa058f771737fa6ac0fecba519dd2d422
alfianvx <devsroot8@gmail.com>
2024-04-15 16:47:38 +0700

    fix: change content from constanct file

--- src/components/card-slider.tsx
+++ src/components/card-slider.tsx
@@ -4,22 +4,26 @@ import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { places } from "@/data/place";
 import Link from "next/link";
+import { RecommendPlace } from "@/constant/Place";
 
 export default function CardSlider() {
   const settings = {
     centerMode: true,
     className: "center",
     centerPadding: "60px",
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 7000,
     slidesToShow: 1,
+    // variableWidth: true,
   };
 
   return (
     <Slider {...settings}>
-      {places.map((place, index) => (
+      {RecommendPlace.map((place, index) => (
         <div
-          className="p-4 w-52 border rounded-lg h-60 bg-gradient-to-bl from-slate-400 to-slate-200"
+          className="p-4 rounded-lg w-52 h-72 bg-zinc-300/50 backdrop-blur-lg"
           key={index}
         >
           <div className="flex flex-col items-start justify-end h-full">
@@ -27,7 +31,14 @@ export default function CardSlider() {
               {place.name}
             </Link>
             <div className="flex items-center space-x-2">
-              <span className="text-xs">{place.location}</span>
+              {place.categories.map((category, index) => (
+                <div
+                  key={index}
+                  className="px-2 py-1 text-xs font-semibold rounded-full bg-white/50 text-zinc-700 backdrop-blur-lg"
+                >
+                  {category.name}
+                </div>
+              ))}
             </div>
           </div>
         </div>
