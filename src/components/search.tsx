e3322b13047387030be5dfc6e0350767990fa8fb
alfianvx <devsroot8@gmail.com>
2024-04-15 16:45:33 +0700

    feat: add search component

---
+++ src/components/search.tsx
@@ -0,0 +1,12 @@
+const Search = () => {
+  return (
+    <div className="mb-5">
+      <input
+        className="w-full p-2 border rounded-md"
+        type="text"
+        placeholder="cari ..."
+      />
+    </div>
+  );
+};
+export default Search;
