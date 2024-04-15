7a9077766452a414f7d775e889e3637c88b95cd9
alfianvx <devsroot8@gmail.com>
2024-04-15 16:46:55 +0700

    feat: add constact to show static vacation place list

---
+++ src/constant/Place.ts
@@ -0,0 +1,69 @@
+export const Places = [
+  {
+    name: "Telaga Menjer",
+    location: "Desa Maron, Garung",
+  },
+  {
+    name: "Curug Winong",
+    location: "Desa Winongsari, Kaliwiro",
+  },
+  {
+    name: "Telaga Warna",
+    location: "Dieng Kulon, Kejajar",
+  },
+  {
+    name: "Embung Kledung",
+    location: "Jalan Raya Parakan, Kledung",
+  },
+  {
+    name: "Lubang Sewu",
+    location: "Desa Erorejo, Wadaslintang",
+  },
+];
+
+export const RecommendPlace = [
+  {
+    name: "Gunung Sikunir",
+    categories: [
+      {
+        name: "Gunung",
+      },
+    ],
+  },
+  {
+    name: "Telaga Menjer",
+    categories: [
+      {
+        name: "Danau",
+      },
+
+      {
+        name: "Taman",
+      },
+    ],
+  },
+  {
+    name: "Curug Sikarim",
+    categories: [
+      {
+        name: "Air Terjun",
+      },
+    ],
+  },
+  {
+    name: "Kebun Teh Tambi",
+    categories: [
+      {
+        name: "Kebun",
+      },
+    ],
+  },
+  {
+    name: "Hutan Pinus Mangli",
+    categories: [
+      {
+        name: "Hutan",
+      },
+    ],
+  },
+];
