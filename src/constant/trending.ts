0675c46a3c4bba8dca3caf645ce86506d30de6db
alfianvx <devsroot8@gmail.com>
2024-06-25 07:57:00 +0700

    feat: add const variabel

---
+++ src/constant/trending.ts
@@ -0,0 +1,14 @@
+export const trending = [
+  {
+    name: "Gunung Prau",
+    link: "/detail-wisata/6",
+  },
+  {
+    name: "Bukit Sikunir",
+    link: "/detail-wisata/5",
+  },
+  {
+    name: "Curug Sikarim",
+    link: "/detail-wisata/3",
+  },
+] as const;
