551d6f4f9eb682bd0f40b24790262169a3ec8a4f
alfianvx <devsroot8@gmail.com>
2024-06-25 07:57:26 +0700

    feat: add types

---
+++ src/types/place.ts
@@ -0,0 +1,10 @@
+export interface Place {
+  id: string;
+  nama: string;
+  alamat: string;
+  deskripsi: string;
+  gambar: string[];
+  harga_tiket: number;
+  jam_operasional: string;
+  kategori: [];
+}
