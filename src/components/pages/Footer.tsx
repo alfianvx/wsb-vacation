ab105811670c6504e252f2fcd3bff8e6243aed2f
alfianvx <devsroot8@gmail.com>
2024-06-25 08:20:45 +0700

    fix: change font style

--- src/components/pages/Footer.tsx
+++ src/components/pages/Footer.tsx
@@ -4,7 +4,9 @@ import { Button } from "../ui/button";
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-between px-2 py-10 space-y-2 lg:flex-row">
-      <h1 className="font-semibold text-lg">wonosobo@wisata.com</h1>
+      <h1 className="font-semibold text-base lg:text-lg">
+        wonosobo@wisata.com
+      </h1>
       <div className="flex items-center">
         <Button asChild variant="link" className="p-2">
           <Link href="/">beranda</Link>
